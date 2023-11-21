b3e5a76da70ca2f7d646ffc6049c31a03ac1c0df
20014606 <151640514+20014606@users.noreply.github.com>
2023-11-21 13:59:16 +0000

    indentation

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask
-   app = Flask(__name__)
-   @app.route('/', methods['GET']) #routing it to the home page
-   def home():
+
+app = Flask(__name__)
+@app.route('/', methods['GET']) #routing it to the home page
+def home():
     return "hello world"
-   app.run(port=5000, debug=true) #function call by the app
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=true) #function call by the app
