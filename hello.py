87b6362a49bb9c5d70f42b9a408c96f782349258
20014606 <151640514+20014606@users.noreply.github.com>
2023-11-21 13:55:13 +0000

    chnge

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
-   app = Flask(__name__) # creating app
+   app = Flask(__name__)
    @app.route('/', methods['GET']) #routing it to the home page
-   def home(): #function
-       return "hello world"
+   def home():
+    return "hello world"
    app.run(port=5000, debug=true) #function call by the app
